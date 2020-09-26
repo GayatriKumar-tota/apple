0245b5b7a2747b06452a0ec6fc8b82105014c9b7
kumar tota <hello@gayathrikumar.com>
2020-09-26 13:52:54 +0800

    changes commited.

---
+++ precache-manifest.ae247669a2ddb4f0b88bcc90f6ad4098.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d8e1e1617abc242ebec32f4b367e7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01675817ea5863450fa6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "78f0547e9ca5282671b6",
+    "url": "/static/js/2.94026f51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94026f51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01675817ea5863450fa6",
+    "url": "/static/js/main.1300d2c9.chunk.js"
+  },
+  {
+    "revision": "08257c312d20d056beeb",
+    "url": "/static/js/runtime-main.6beaef57.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
